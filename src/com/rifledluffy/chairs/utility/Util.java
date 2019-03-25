a419108c644b3f9f03d31d5e7715fb229f49d577
Carl Poole <email@carl.codes>
2019-03-25 01:24:39 -0400

    Applies changes to armorstand entity before spawning, prevents armor stand flashing

--- src/com/rifledluffy/chairs/utility/Util.java
+++ src/com/rifledluffy/chairs/utility/Util.java
@@ -259,14 +259,14 @@ public class Util {
 		
 		Double height = 0.25D;
 		if (Util.isCarpetBlock(chair.getBlock().getType())) height = 0D;
-		
-		ArmorStand armorStand = (ArmorStand)seat.getWorld().spawnEntity(seat.add(0.5, height, 0.5).setDirection(vector), EntityType.ARMOR_STAND);
-		
-		armorStand.setVisible(false);
-		armorStand.setGravity(false);
-		armorStand.setInvulnerable(true);
-		armorStand.setMarker(true);
-		armorStand.setCollidable(false);
+
+        ArmorStand armorStand = (ArmorStand) seat.getWorld().spawn(seat.add(0.5, height, 0.5).setDirection(vector), ArmorStand.class, stand -> {
+                stand.setVisible(false);
+                stand.setGravity(false);
+                stand.setInvulnerable(true);
+                stand.setMarker(true);
+                stand.setCollidable(false);
+            });
 		
 		return armorStand;
 	}
